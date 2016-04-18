c96a1eb67b78ab6f3a8474aace931f59caf0c9e0
Jean-Jacques MOIROUX <jeanjacquesmoiroux@hotmail.com>
2016-04-18 20:09:28 +0200

    rotation X Y, translation Y OK

--- Assets/Script/moveMouse.cs
+++ Assets/Script/moveMouse.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+enum Dir : int {
+	ROTATEUP,
+	ROTATEDOWN,
+	ROTATELEFT,
+	ROTATERIGHT,
+	TRANSUP,
+	TRANSDOWN,
+	TRANSLEFT,
+	TRANSRIGHT
+}
+
 public class moveMouse : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject	itemObject;
+
 	void Start () {
-	
+		this.itemObject = GameObject.Find ("logo-4");
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		mouseEvent ();
 	}
@@ -17,23 +28,40 @@ public class moveMouse : MonoBehaviour {
 		if (Input.GetMouseButton (0)) {
 			if ((Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl))) { // ROTATE VERTICAL
 				if (Input.GetAxis("Mouse Y") > 0){
-					Debug.Log("MOUSE AXE UP");
+					do_Y_rotate((int)Dir.ROTATEUP);
 				} else if (Input.GetAxis("Mouse Y") < 0){
-					Debug.Log("MOUSE AXE DOWN");
+					do_Y_rotate((int)Dir.ROTATEDOWN);
 				}
 			} else if ((Input.GetKey (KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))) { // TRANSLATE VERTICAL
-				if (Input.GetAxis("Mouse X") < 0){
-					Debug.Log("MOUSE AXE LEFT");
-				} else if (Input.GetAxis("Mouse X") > 0){
-					Debug.Log("MOUSE AXE RIGHT");
+				if (Input.GetAxis("Mouse Y") < 0){
+					do_VERT_translate((int)Dir.TRANSUP);
+				} else if (Input.GetAxis("Mouse Y") > 0){
+					do_VERT_translate((int)Dir.TRANSDOWN);
 				}
 			} else { // ROTATE HONRIZONTAL
 				if (Input.GetAxis("Mouse X") < 0){
-					Debug.Log("MOUSE AXE LEFT");
+					do_X_rotate((int)Dir.ROTATELEFT);
 				} else if (Input.GetAxis("Mouse X") > 0){
 					Debug.Log("MOUSE AXE RIGHT");
 				}
 			}
 		}
+	}
+
+	private void do_X_rotate(int direction){
+		float rotate = (direction == (int)Dir.ROTATELEFT) ? 40.0f : -40.0f;
+		this.itemObject.transform.Rotate( new Vector3( 0.0f, rotate, 0.0f) * Time.deltaTime );
+
+	}
+
+	private void do_Y_rotate(int direction){
+		float rotate = (direction == (int)Dir.ROTATEUP) ? 40.0f : -40.0f;
+		this.itemObject.transform.Rotate( new Vector3( rotate, 0.0f, 0.0f) * Time.deltaTime );
+		
+	}
+
+	private void do_VERT_translate(int direction) {
+		float translate = (direction == (int)Dir.TRANSUP) ? -20.0f : 20.0f;
+		this.itemObject.transform.position += new Vector3 (0.0f, translate, 0.0f) * Time.deltaTime;
 	}
 }
