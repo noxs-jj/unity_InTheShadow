05383d60876c67b36625ffc77206585ab8fb80fd
Jean-Jacques MOIROUX <jeanjacquesmoiroux@hotmail.com>
2016-04-20 18:33:53 +0200

    music d'ambiance

--- Assets/Script/uiManager.cs
+++ Assets/Script/uiManager.cs
@@ -1,17 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class uiManager : MonoBehaviour {
 
+	private GameObject	buttonClassicModeObject;
+	private GameObject	buttonTestModeObject;
+	private GameObject	panelLevelSelectObject;
+	private GameObject	maskTeaPotObject;
+	private GameObject	maskElephantObject;
+	private GameObject	maskGlobeObject;
+	private GameObject	maskTBonusObject;
+
 	void Start () {
-	
+		this.buttonClassicModeObject = GameObject.Find ("button_classic_mode");
+		this.buttonTestModeObject = GameObject.Find ("button_test_mode");
+		this.panelLevelSelectObject = GameObject.Find ("panel_level_select");
+		this.maskTeaPotObject = GameObject.Find ("mask_image_teaPot");
+		this.maskElephantObject = GameObject.Find ("mask_image_elephant");
+		this.maskGlobeObject = GameObject.Find ("mask_image_globe");
+		this.maskTBonusObject = GameObject.Find ("mask_image_bonus");
+
+		this.panelLevelSelectObject.SetActive (false);
 	}
 	
 	void Update () {
 	
 	}
 
-	void launch_level(){
-
-	}
+	public void activateTestPanelSelect(){ this.panelLevelSelectObject.SetActive (true); }
+	public void desactivateTestPanelSelect(){ this.panelLevelSelectObject.SetActive (false); }
+	public void activateClassicPanelSelect(){ this.panelLevelSelectObject.SetActive (true); }
+	public void desactivateClassicPanelSelect(){ this.panelLevelSelectObject.SetActive (false); }
 }
