6715bc80e9c357eea3f8913806261ba1a30cb27b
Jean-Jacques MOIROUX <jeanjacquesmoiroux@hotmail.com>
2016-04-22 19:32:05 +0200

    v000004 with credits

---
+++ Assets/Script/logoIntro.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class logoIntro : MonoBehaviour {
+	private float 	i = 0.0f;
+	private bool	isGoUp = true;
+	
+	void Update () {
+		if (false == this.isGoUp) {
+			gameObject.transform.position -= new Vector3 (0.0f, this.i, 0.0f) * Time.deltaTime;
+			gameObject.transform.position -= new Vector3 (this.i, 0.0f, 0.0f) * Time.deltaTime;
+			this.i -= 0.3f;
+		} else if (true == this.isGoUp) {
+			this.i += 0.3f;
+			gameObject.transform.position += new Vector3 (0.0f, this.i, 0.0f) * Time.deltaTime;
+			gameObject.transform.position += new Vector3 (this.i, 0.0f, 0.0f) * Time.deltaTime;
+		}
+		if (this.i >= 20.0f) {
+			this.isGoUp = false;
+		} else if (this.i <= -20.0f) {
+			this.isGoUp = true;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# In The Shadow
+
+### 42 Born2Code Project
+
+#### build with Unity 5.2.2f1
+
+
+#### Credits
+jmoiroux @42Born2code
+
+twitter@jjmoiroux
+
+jjmoiroux@gmail.com
